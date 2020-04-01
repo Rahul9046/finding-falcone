b0511184b85c6015c01438665ef9ccbf82043144
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2020-04-01 12:55:09 +0530

    services are set up

---
+++ src/services/constant.js
@@ -0,0 +1,9 @@
+// decalare all constants
+const URL = 'https://findfalcone.herokuapp.com/',
+    VEHICLES = 'vehicles',
+    FIND = 'find',
+    PLANETS = 'planets',
+    TOKEN = 'token';
+
+// export all constants that will be used inside apis 
+export { URL, VEHICLES, FIND, PLANETS, TOKEN}
